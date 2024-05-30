7006f44c0fb0e3e689e9d9e7c41f94f6c7453097
Bruno Fernando <brunofernandodco@gmail.com>
2024-05-30 18:23:20 -0300

    Lancamento init

---
+++ src/main/java/br/com/fiap/fintech/jdbc/DatabaseManager.java
@@ -0,0 +1,5 @@
+package br.com.fiap.fintech.jdbc;
+
+public class DatabaseManager {
+
+}
---
+++ src/main/java/br/com/fiap/fintech/model/bean/Lancamento.java
@@ -0,0 +1,83 @@
+package br.com.fiap.fintech.model.bean;
+
+import java.time.LocalDate;
+
+public abstract class Lancamento {
+
+	private Integer codigoDoLancamento;
+	private Integer numeroDoCPF;
+	private LocalDate dtDoLancamento;
+	private LocalDate hrDoLancamento;
+	private Double vlDoLancamento;
+	private String dsDoLancamento;
+	
+	public Lancamento() {
+		
+	}
+	
+	public Lancamento(Integer codigoDoLancamento, Integer numeroDoCPF, LocalDate dtDoLancamento,
+			LocalDate hrDoLancamento, Double vlDoLancamento, String dsDoLancamento) {
+		this.codigoDoLancamento = codigoDoLancamento;
+		this.numeroDoCPF = numeroDoCPF;
+		this.dtDoLancamento = dtDoLancamento;
+		this.hrDoLancamento = hrDoLancamento;
+		this.vlDoLancamento = vlDoLancamento;
+		this.dsDoLancamento = dsDoLancamento;
+	}
+
+	public Integer getCodigoDoLancamento() {
+		return codigoDoLancamento;
+	}
+
+	public void setCodigoDoLancamento(Integer codigoDoLancamento) {
+		this.codigoDoLancamento = codigoDoLancamento;
+	}
+
+	public Integer getNumeroDoCPF() {
+		return numeroDoCPF;
+	}
+
+	public void setNumeroDoCPF(Integer numeroDoCPF) {
+		this.numeroDoCPF = numeroDoCPF;
+	}
+
+	public LocalDate getDtDoLancamento() {
+		return dtDoLancamento;
+	}
+
+	public void setDtDoLancamento(LocalDate dtDoLancamento) {
+		this.dtDoLancamento = dtDoLancamento;
+	}
+
+	public LocalDate getHrDoLancamento() {
+		return hrDoLancamento;
+	}
+
+	public void setHrDoLancamento(LocalDate hrDoLancamento) {
+		this.hrDoLancamento = hrDoLancamento;
+	}
+
+	public Double getVlDoLancamento() {
+		return vlDoLancamento;
+	}
+
+	public void setVlDoLancamento(Double vlDoLancamento) {
+		this.vlDoLancamento = vlDoLancamento;
+	}
+
+	public String getDsDoLancamento() {
+		return dsDoLancamento;
+	}
+
+	public void setDsDoLancamento(String dsDoLancamento) {
+		this.dsDoLancamento = dsDoLancamento;
+	}
+
+	@Override
+	public String toString() {
+		return "Lancamento [codigoDoLancamento=" + codigoDoLancamento + ", numeroDoCPF=" + numeroDoCPF
+				+ ", dtDoLancamento=" + dtDoLancamento + ", hrDoLancamento=" + hrDoLancamento + ", vlDoLancamento="
+				+ vlDoLancamento + ", dsDoLancamento=" + dsDoLancamento + "]";
+	}
+	
+}
